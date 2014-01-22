3c3d2f03d84e692471da95201097c560cc6cf731
Apprentice <apprentice@DBC-Infinity-2.local>
2014-01-22 11:35:31 -0800

    extracted private methods

--- source/activerecord_jr/models/cohort.rb
+++ source/activerecord_jr/models/cohort.rb
@@ -3,7 +3,6 @@ class Cohort < Database::Model
 
   attr_reader :attributes, :old_attributes
 
-
   def students
     Student.where('cohort_id = ?', self[:id])
   end
@@ -12,40 +11,6 @@ class Cohort < Database::Model
     students.each do |student|
       student.cohort = self
     end
-
     students
-  end
-
-
-
-  private
-  def insert!
-    self[:created_at] = DateTime.now
-    self[:updated_at] = DateTime.now
-
-    fields = self.attributes.keys
-    values = self.attributes.values
-    marks  = Array.new(fields.length) { '?' }.join(',')
-
-    insert_sql = "INSERT INTO cohorts (#{fields.join(',')}) VALUES (#{marks})"
-
-    results = Database::Model.execute(insert_sql, *values)
-
-    # This fetches the new primary key and updates this instance
-    self[:id] = Database::Model.last_insert_row_id
-    results
-  end
-
-  def update!
-    self[:updated_at] = DateTime.now
-
-    fields = self.attributes.keys
-    values = self.attributes.values
-
-    update_clause = fields.map { |field| "#{field} = ?" }.join(',')
-    update_sql = "UPDATE cohorts SET #{update_clause} WHERE id = ?"
-
-    # We have to use the (potentially) old ID attributein case the user has re-set it.
-    Database::Model.execute(update_sql, *values, self.old_attributes[:id])
   end
 end
--- source/activerecord_jr/models/student.rb
+++ source/activerecord_jr/models/student.rb
@@ -12,36 +12,4 @@ class Student < Database::Model
     self.save
     cohort
   end
-
-  private
-
-  def insert!
-    self[:created_at] = DateTime.now
-    self[:updated_at] = DateTime.now
-
-    fields = self.attributes.keys
-    values = self.attributes.values
-    marks  = Array.new(fields.length) { '?' }.join(',')
-
-    insert_sql = "INSERT INTO students (#{fields.join(',')}) VALUES (#{marks})"
-
-    results = Database::Model.execute(insert_sql, *values)
-
-    # This fetches the new primary key and updates this instance
-    self[:id] = Database::Model.last_insert_row_id
-    results
-  end
-
-  def update!
-    self[:updated_at] = DateTime.now
-
-    fields = self.attributes.keys
-    values = self.attributes.values
-
-    update_clause = fields.map { |field| "#{field} = ?" }.join(',')
-    update_sql = "UPDATE students SET #{update_clause} WHERE id = ?"
-
-    # We have to use the (potentially) old ID attribute in case the user has re-set it.
-    Database::Model.execute(update_sql, *values, self.old_attributes[:id])
-  end
 end
